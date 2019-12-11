05d7f1e0556eabce0b538a1c154becb7efef664d
dblancoformacion <dblancoformacion@gmail.com>
2019-12-11 09:32:03 +0100

    Provincias

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-David (profe)
+<?php // Vamos a realizar las consultas del entrenador en php
+
+$conn = new mysqli('localhost','root','','provincias');
+$conn->query("SET NAMES utf8;");
+
+$provincias=$conn->query("
+	SELECT * FROM provincias;
+")->fetch_all(MYSQLI_ASSOC);
+
+if(0){
+	echo '<pre>';
+	print_r($provincias);
+	echo '</pre>';
+}
+
+if(1){ // provincias de Galicia
+	foreach($provincias as $p){
+		if($p['autonomia']=='Galicia'){
+			echo $p['provincia'];
+			echo '<br/>';
+		}
+	}
+}
