aba15a675e3406fa47bcaec27e43eebccc931963
dblancoformacion <dblancoformacion@gmail.com>
2019-12-11 09:47:37 +0100

    Provincias que empiezan por A

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php // Vamos a realizar las consultas del entrenador en php
+if(1){	// conexión a la base de datos
+	$conn = new mysqli('localhost','root','','provincias');
+	$conn->query("SET NAMES utf8;");
 
-$conn = new mysqli('localhost','root','','provincias');
-$conn->query("SET NAMES utf8;");
-
-$provincias=$conn->query("
-	SELECT * FROM provincias;
-")->fetch_all(MYSQLI_ASSOC);
-
-if(0){
+	$provincias=$conn->query("
+		SELECT * FROM provincias;
+	")->fetch_all(MYSQLI_ASSOC);
+}
+if(0){	// tabla completa
 	echo '<pre>';
 	print_r($provincias);
 	echo '</pre>';
 }
-
-if(1){ // provincias de Galicia
+if(0){ 	// provincias de Galicia
 	foreach($provincias as $p){
 		if($p['autonomia']=='Galicia'){
 			echo $p['provincia'];
 			echo '<br/>';
 		}
 	}
+}
+if(1){	// provincias que empiezan por A
+	foreach($provincias as $p)
+		if($p['provincia'][0]=='A')
+			echo $p['provincia'].'<br/>';
 }
